53106a1fbbb7b9dcb6fd6acd0c941b019766ec55
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-09-14 22:06:08 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Olesia Libra and is {''}
-        <a href="https://github.com/" target="_blank" rel="noreferrer">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Olesia Libra and is {''}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          <button class="btn btn-primary">Button</button>
+        </footer>
+      </div>
     </div>
   );
 }
